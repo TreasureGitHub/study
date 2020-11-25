f4cd628d0d1a3c307fa6f02ffd6e564891d9d70a
TreasureGitHub <lf20061261@163.com>
2020-11-25 09:11:40 +0800

    commit

--- study-spark/src/main/scala/com/ffl/study/spark/scala/sparkcore/OperatorValue.scala
+++ study-spark/src/main/scala/com/ffl/study/spark/scala/sparkcore/OperatorValue.scala
@@ -7,7 +7,7 @@ import org.apache.spark.{SparkConf, SparkContext}
   * @author lff
   * @datetime 2020/11/19 12:45
   */
-object Operator1 {
+object OperatorValue {
 
     def main(args: Array[String]): Unit = {
 
@@ -17,41 +17,41 @@ object Operator1 {
 
         val listRDD: RDD[Int] = sc.parallelize(1 to 10)
 
-        // map
+        // 1.map
         listRDD.map(_ * 2).collect().foreach(println)
 
         // mapPartitions 可以对一个RDD中所有分区进行遍历
         // mapPartitions 效率优于map算子，减少了发送到执行器执行交互数
         // mapPartitions 可能会出现内存溢出(oom)
 
-        // mapPartitions
+        // 2.mapPartitions
         listRDD.mapPartitions(datas => {
             datas.map(_ * 2)
         }).collect().foreach(println)
 
-        // mapPartitionsWithIndex
+        // 3.mapPartitionsWithIndex  ，
         val tupleRDD: RDD[(Int, String)] = listRDD.mapPartitionsWithIndex {
-            case (num, datas) => {
-                datas.map((_, "分区号：" + num))
+            case (num, data) => {
+                data.map((_, "分区号：" + num))
             }
         }
 
         tupleRDD.foreach(println)
         val dataRDD: RDD[List[Int]] = sc.parallelize(Array(List(1, 2), List(3, 4)))
 
-        // flatMap
+        // 4.flatMap
         val resultRDD: RDD[Int] = dataRDD.flatMap(datas => datas)
         resultRDD.collect().foreach(println)
 
         val dataRDD1: RDD[Int] = sc.makeRDD(Array(1, 2, 3, 4, 5, 6, 7, 8), 3)
 
 
-        //glom
+        // 5.glom
         val resultRDD1: RDD[Array[Int]] = dataRDD1.glom()
         resultRDD1.collect().foreach(arr => println(arr.mkString(",")))
 
 
-        // groupBy,分组后的数据形成了 对偶元组(K,V)
+        // 6.groupBy,分组后的数据形成了 对偶元组(K,V)
         val dataRDD2: RDD[Int] = sc.makeRDD(List(1, 2, 3, 4))
 
         val resultRDD2: RDD[(Int, Iterable[Int])] = dataRDD2.groupBy(_ % 2)
@@ -61,12 +61,14 @@ object Operator1 {
         // groupBy,分组后的数据形成了 对偶元组(K,V)
         val dataRDD3: RDD[Int] = sc.makeRDD(List(1, 2, 3, 4))
 
+        //7.filter
         val resultRDD3: RDD[Int] = dataRDD2.filter(_ % 2 == 0)
         resultRDD3.collect().foreach(println)
 
         // 从指定集合中进行抽样处理，根据不同的算法进行抽样
         val dataRDD4: RDD[Int] = sc.makeRDD(1 to 10)
 
+        // 8.sample
         // false 是否放回，
         val resultRDD4: RDD[Int] = dataRDD4.sample(false, 0.6, 1)
         resultRDD4.collect().foreach(println)
@@ -74,11 +76,12 @@ object Operator1 {
         // 从指定集合中进行抽样处理，根据不同的算法进行抽样
         val dataRDD5: RDD[Int] = sc.makeRDD(List(1,2,1,5,2,9,6,1))
 
-        // false 是否放回，
+        // 9.distinct
         val resultRDD5: RDD[Int] = dataRDD5.distinct(2)
         resultRDD5.collect().foreach(println)
 
 
+        // 10 coalesce
         // 缩减分区数:合并分区；没有打乱重组，没有shuffle
         val dataRDD6: RDD[Int] = sc.makeRDD(1 to 16,4)
 
@@ -89,7 +92,7 @@ object Operator1 {
         println("缩减后分区：" + dataRDD7.partitions.size)
 
 
-        // 排序,降序
+        // 11.排序,降序
         dataRDD7.sortBy(x => x,false).collect().foreach(println)
 
         sc.stop()
--- study-spark/src/main/scala/com/ffl/study/spark/scala/sparkcore/RDDCreate.scala
+++ study-spark/src/main/scala/com/ffl/study/spark/scala/sparkcore/RDDCreate.scala
@@ -25,27 +25,35 @@ object RDDCreate {
         //val listRDD: RDD[Int] = sc.makeRDD(List(1, 2, 3, 4),2)
 
         listRDD.collect()
-
         // 输出路径
-        val outPath = PathConstants.SPARK_RES + "/output";
+        val outPath = PathConstants.SPARK_RES + "/rddCreate/listRDD";
         FileUtils.deleteDir(outPath)
         listRDD.saveAsTextFile(outPath)
 
+
         // 1.2从内存中创建  parallelize
         val listRDD1: RDD[Int] = sc.parallelize(Array(1,2,3,4))
-
         //val listRDD1: RDD[Int] = sc.parallelize(Array(1,2,3,4),2)
+        val outPath1 = PathConstants.SPARK_RES + "/rddCreate/listRDD1";
 
         listRDD1.collect().foreach(println)
+        FileUtils.deleteDir(outPath1)
+        listRDD.saveAsTextFile(outPath1)
+
 
         // 2.从文件读取，默认为2
         val fileRDD: RDD[String] = sc.textFile(PathConstants.SPARK_RES + "/input")
 
         // 指定分区数
         // 读取文件时，传递的分区参数为最小分区数，但是不一定是分区数，取决于hadoop读取文件时的分区规则
-        //val fileRDD: RDD[String] = sc.textFile(PathConstants.SPARK_RES + "/input",3)
+        val fileRDD1: RDD[String] = sc.textFile(PathConstants.SPARK_RES + "/input",2)
 
         fileRDD.collect().foreach(println)
+        val outPath2 = PathConstants.SPARK_RES + "/rddCreate/fileRDD";
+        FileUtils.deleteDir(outPath2)
+        fileRDD1.saveAsTextFile(outPath2)
+
+        sc.stop()
 
     }
 
